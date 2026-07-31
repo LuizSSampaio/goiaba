ce03b843dbda243fc60af75d9dfc02d4cb4d13a4
Luiz Henrique Silva Sampaio <luiz@lsamp.dev>
2026-07-31 10:11:36 -0300

    feat(vulkan): device selection

--- modules/renderer/src/backend/vulkan/Vulkan.cpp
+++ modules/renderer/src/backend/vulkan/Vulkan.cpp
@@ -1,8 +1,11 @@
 #include "Vulkan.hpp"
 
 #include <GE/Logger.hpp>
-
-#include "vulkan/vulkan.hpp"
+#include <algorithm>
+#include <cstdint>
+#include <expected>
+#include <map>
+#include <utility>
 
 using namespace GE::Render::Backend;
 
@@ -17,13 +20,13 @@ std::expected<void, Vulkan::Error> Vulkan::Init(
             "Vulkan Backend failed creating a instance with code: " +
                 std::to_string(static_cast<uint32_t>(instanceRes.error())),
             Logger::Engine);
-        return std::unexpected(Error::FailedInstanceCreation);
+        return std::unexpected(instanceRes.error());
     }
 
     return {};
 }
 
-std::expected<void, vk::Result> Vulkan::CreateInstance(
+std::expected<void, Vulkan::Error> Vulkan::CreateInstance(
     const std::string& appName, const std::string& engineName,
     const Extensions& extensions) {
     vk::ApplicationInfo appInfo{
@@ -41,8 +44,66 @@ std::expected<void, vk::Result> Vulkan::CreateInstance(
     vk::Result res = vk::createInstance(&instanceCI, nullptr, &instance);
 
     if (res != vk::Result::eSuccess) {
-        return std::unexpected(res);
+        return std::unexpected(Vulkan::Error::FailedInstanceCreation);
     }
     this->instance_ = instance;
     return {};
+}
+
+std::expected<void, Vulkan::Error> Vulkan::SelectDevice() {
+    vk::PhysicalDevice physicalDevice = nullptr;
+    auto physicalDevices = this->instance_.enumeratePhysicalDevices();
+    if (physicalDevices.empty()) {
+        Logger::Critical("Failed to find a GPU with Vulkan support",
+                         Logger::Engine);
+        return std::unexpected(Vulkan::Error::NoVulkanDevice);
+    }
+
+    auto bestDevice = Vulkan::PickBestDevice(physicalDevices);
+    if (!bestDevice.has_value()) {
+        return std::unexpected(bestDevice.error());
+    }
+    physicalDevice = bestDevice.value();
+
+    return {};
+}
+
+std::expected<vk::PhysicalDevice, Vulkan::Error> Vulkan::PickBestDevice(
+    const std::vector<vk::PhysicalDevice>& devices) {
+    std::multimap<int, vk::PhysicalDevice> candidates;
+    for (const auto& device : devices) {
+        uint32_t score = 0;
+
+        auto deviceProperties = device.getProperties();
+        if (deviceProperties.deviceType ==
+            vk::PhysicalDeviceType::eDiscreteGpu) {
+            constexpr int discreteGpuScore = 1000;
+            score += discreteGpuScore;
+        }
+
+        score += deviceProperties.limits.maxImageDimension2D;
+
+        auto deviceFeatures = device.getFeatures();
+        if (!static_cast<bool>(deviceFeatures.geometryShader)) {
+            continue;
+        }
+
+        auto deviceQueueFamilies = device.getQueueFamilyProperties();
+        bool supportsGraphics = std::ranges::any_of(
+            deviceQueueFamilies, [](auto const& queueFamilyProperties) {
+                return static_cast<bool>(queueFamilyProperties.queueFlags &
+                                         vk::QueueFlagBits::eGraphics);
+            });
+        if (!supportsGraphics) {
+            continue;
+        }
+
+        candidates.insert(std::make_pair(score, device));
+    }
+
+    if (candidates.empty() || candidates.rbegin()->first <= 0) {
+        return std::unexpected(Vulkan::Error::NoSuitableDevice);
+    }
+
+    return candidates.rbegin()->second;
 }
--- modules/renderer/src/backend/vulkan/Vulkan.hpp
+++ modules/renderer/src/backend/vulkan/Vulkan.hpp
@@ -14,7 +14,11 @@
 namespace GE::Render::Backend {
 class Vulkan : public IBackend {
 public:
-    enum Error { FailedInstanceCreation };
+    enum Error {
+        FailedInstanceCreation,
+        NoVulkanDevice,
+        NoSuitableDevice,
+    };
 
     struct Extensions {
         char const* const* names;
@@ -31,8 +35,12 @@ public:
 private:
     vk::Instance instance_;
 
-    std::expected<void, vk::Result> CreateInstance(
-        const std::string& appName, const std::string& engineName,
-        const Extensions& extensions);
+    std::expected<void, Error> CreateInstance(const std::string& appName,
+                                              const std::string& engineName,
+                                              const Extensions& extensions);
+
+    std::expected<void, Error> SelectDevice();
+    static std::expected<vk::PhysicalDevice, Vulkan::Error> PickBestDevice(
+        const std::vector<vk::PhysicalDevice>& devices);
 };
 }  // namespace GE::Render::Backend
