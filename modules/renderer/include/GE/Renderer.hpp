6095b1923c92b168d0c1d402feb76204952e0734
Luiz Henrique Silva Sampaio <luiz@lsamp.dev>
2026-08-02 11:40:40 -0300

    feat(renderer): start backend init impl

--- modules/renderer/include/GE/Renderer.hpp
+++ modules/renderer/include/GE/Renderer.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
+#include <cstdint>
+#include <expected>
+
 namespace GE::Render {
 class Renderer {
 public:
-    Renderer();
-    ~Renderer();
+    enum Error : uint8_t {
+        FailedToInitializeBackend,
+    };
 
+    enum Backend : uint8_t {
+        Vulkan,
+    };
+
+    Renderer() = default;
+    ~Renderer() = default;
+
+    std::expected<void, Error> Init(Backend backend);
     void Run();
+
+private:
+    std::expected<void, Error> InitVulkan();
 };
 }  // namespace GE::Render
--- modules/renderer/src/Renderer.cpp
+++ modules/renderer/src/Renderer.cpp
@@ -3,17 +3,42 @@
 
 #include <SDL3/SDL_vulkan.h>
 
+#include <GE/Logger.hpp>
+#include <string>
+
 #include "./backend/vulkan/Vulkan.hpp"
 
 using namespace GE::Render;
 
-Renderer::Renderer() {
-    Backend::Vulkan backend = Backend::Vulkan();
-    Backend::Vulkan::Extensions extensions;
-    extensions.names = SDL_Vulkan_GetInstanceExtensions(&extensions.count);
-    backend.Init("Sample", "Goiaba", extensions);
+std::expected<void, Renderer::Error> Renderer::Init(Renderer::Backend backend) {
+    switch (backend) {
+        case Renderer::Backend::Vulkan:
+            auto vkRes = this->InitVulkan();
+            if (!vkRes.has_value()) {
+                return std::unexpected(vkRes.error());
+            }
+            break;
+    }
+
+    return {};
 }
 
-Renderer::~Renderer() {}
+std::expected<void, Renderer::Error> Renderer::InitVulkan() {
+    GE::Render::Backend::Vulkan backend = GE::Render::Backend::Vulkan();
+    GE::Render::Backend::Vulkan::Extensions extensions;
+    extensions.names = SDL_Vulkan_GetInstanceExtensions(&extensions.count);
+
+    auto backendRes = backend.Init("Sample", "Goiaba", extensions);
+    if (!backendRes.has_value()) {
+        Logger::Critical("Failed to initialize Vulkan Backend(Error: " +
+                             std::to_string(backendRes.error()) + ")",
+                         Logger::Engine);
+        return std::unexpected(Renderer::Error::FailedToInitializeBackend);
+    }
+
+    // TODO: Create window and surface
+
+    return {};
+}
 
 void Renderer::Run() {}
