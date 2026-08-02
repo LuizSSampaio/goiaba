21cb84e52490eea48b70886273d1548b0ad48e82
Luiz Henrique Silva Sampaio <luiz@lsamp.dev>
2026-08-02 15:32:46 -0300

    fix(window): use bitshift for bitflag

--- modules/renderer/include/GE/Window.hpp
+++ modules/renderer/include/GE/Window.hpp
@@ -1,39 +1,46 @@
 #pragma once
 
 #include <cstdint>
+#include <expected>
 #include <string>
 
 namespace GE::Render {
 class Window {
 public:
-    enum class Flag : uint8_t {
-        Fullscreen,
-        Occluded,
-        Hidden,
-        Borderless,
-        Resizable,
-        Minimized,
-        Maximized,
-        MouseGrabbed,
-        InputFocus,
-        MouseFocus,
-        Modal,
-        HighPixelDensity,
-        MouseCapture,
-        MouseRelativeMode,
-        AlwaysOnTop,
-        Utility,
-        Tooltip,
-        PopupMenu,
-        KeyboardGrabbed,
-        Transparent,
-        NotFocusable,
+    enum Error : uint8_t {
+        FailToCreateWindow,
+    };
+
+    enum class Flag : uint32_t {
+        Fullscreen = 1 << 0,
+        Occluded = 1 << 1,
+        Hidden = 1 << 2,
+        Borderless = 1 << 3,
+        Resizable = 1 << 4,
+        Minimized = 1 << 5,
+        Maximized = 1 << 6,
+        MouseGrabbed = 1 << 7,
+        InputFocus = 1 << 8,
+        MouseFocus = 1 << 9,
+        Modal = 1 << 10,
+        HighPixelDensity = 1 << 11,
+        MouseCapture = 1 << 12,
+        MouseRelativeMode = 1 << 13,
+        AlwaysOnTop = 1 << 14,
+        Utility = 1 << 15,
+        Tooltip = 1 << 16,
+        PopupMenu = 1 << 17,
+        KeyboardGrabbed = 1 << 18,
+        Transparent = 1 << 19,
+        NotFocusable = 1 << 20,
     };
 
     virtual ~Window() = default;
 
-    virtual Window CreateWindow(const std::string& title, uint32_t width,
-                                uint32_t height, uint8_t flags) = 0;
+    virtual std::expected<void, Error> InitWindow(const std::string& title,
+                                                  uint32_t width,
+                                                  uint32_t height,
+                                                  Flag windowFlags) = 0;
 
     virtual std::string title() = 0;
     virtual void SetTitle(const std::string& title) = 0;
