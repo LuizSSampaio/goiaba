44b6a6e3dfce20b4ba25f6f9db425a2829390864
Luiz Henrique Silva Sampaio <luiz@lsamp.dev>
2026-08-11 11:36:20 -0300

    fix(renderer): add missing ShaderData struct

---
+++ modules/renderer/src/ShaderData.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <GE/Math.hpp>
+#include <cstdint>
+
+struct ShaderData {
+    GE::Math::Mat4 projection;
+    GE::Math::Mat4 view;
+    std::array<GE::Math::Mat4, 3> model;
+    // TODO: Remove hardcoded light position
+    GE::Math::Vec4 lightPos = {0.0f, -10.0f, 10.0f, 0.0f};
+    uint32_t selected = 1;
+};
