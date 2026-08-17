bd005168b40b25c693d7ec5ce7a8d05cf49cf2ec
Luiz Henrique Silva Sampaio <luiz@lsamp.dev>
2026-08-17 14:14:56 -0300

    feat(platform): implement surface wrappers

---
+++ modules/platform/include/GE/Platform/SurfaceFactory.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+#include <expected>
+#include <memory>
+#include <vector>
+
+#include "Surface.hpp"
+#include "Window.hpp"
+
+namespace GE::Platform {
+class SurfaceFactory {
+public:
+    virtual ~SurfaceFactory() = default;
+
+    virtual std::vector<const char*> RequiredInstanceExtensions() = 0;
+    virtual std::expected<std::unique_ptr<Surface>, Surface::Error>
+    CreateSurface(Window& window, uint64_t instanceHandle) = 0;
+};
+}  // namespace GE::Platform
